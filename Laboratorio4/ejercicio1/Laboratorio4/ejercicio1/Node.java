c72b82657877dd31237d85ddf788087099007547
OscarChilo <ochilo@unsa.edu.pe>
2023-06-27 12:31:26 -0500

    update

--- Laboratorio4/ejercicio1/Laboratorio4/ejercicio1/Node.java
+++ Laboratorio4/ejercicio1/Laboratorio4/ejercicio1/Node.java
@@ -9,29 +9,32 @@ public class Node<T> {
     private T data;
     private Node<T> nextNode;
 
-    public Node(){
+    public Node() {
     }
 
-    public Node(T data){
-        this.data=data;
+    public Node(T data) {
+        this.data = data;
     }
 
     public Node(T data, Node<T> nextNode) {
-        this.data=data;
-        this.nextNode=nextNode;
+        this.data = data;
+        this.nextNode = nextNode;
     }
-    
-    public T getData(){
+
+    public T getData() {
         return this.data;
     }
+
     public void setData(T data) {
-        this.data=data;
+        this.data = data;
     }
-    public Node getNextNode(){
+
+    public Node getNextNode() {
         return this.nextNode;
     }
+
     public void setNextNode(Node<T> nextnNode) {
-        this.nextNode=nextNode;
+        this.nextNode = nextNode;
     }
-    
+
 }
